5dd68b1232a99ede3b5e3b6a6a0540716822c569
Eleprolu Durga prasad <eleproludurgaprasad@gmail.com>
2025-09-25 03:28:35 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3818039381-cfe74",
+  "appId": "1:40826974286:web:fa58395ffc3ff86211531f",
+  "apiKey": "AIzaSyD9wjAQE99aUWTrnaKpdSlpcUqN6Y0oatA",
+  "authDomain": "studio-3818039381-cfe74.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "40826974286"
+};
